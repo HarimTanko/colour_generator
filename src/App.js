6f1ca448d5b2331581452cc56faeed1283f7b9cf
Harim Tanko <harimtanko@gmail.com>
2022-02-12 23:41:48 +0000

    set functionality for extracting values from input and fetching data

--- src/App.js
+++ src/App.js
@@ -16,16 +16,28 @@ function App() {
 
   //set up a handle submit because were going to use a
   //form
-  const hadleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
+    // according to the documentation, use new Values and assign it to a variable color
+    //then you can call methods on that variable color
+    //pass into the Values 'color'what you extracted from the input by setColor(e.target.vale)
+
+    //wrap the the fetching of the colors in a trycatch and set the setError to true to
+    //use it somewhere else (use in the input element)
+    try {
+      let colors = new Values(color).all(10);
+      console.log(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
   };
 
   return (
     <>
       <section className="container">
         <h3>Colour Generator</h3>
-        <form onsSubmit={hadleSubmit}>
+        <form onSubmit={handleSubmit}>
           {/** value is equal to color because we want to set the
            * state value to be equal to the value of the input
            * that is how you control it.
@@ -37,6 +49,11 @@ function App() {
             value={color}
             onChange={(e) => setColor(e.target.value)}
             placeholder="#f15025"
+            /** using the ternary operator set the classname to 'error' if error is true
+             * this was set in the setError in the trycatch when tryin to extract the
+             * input value and setting it to the new Values from the docs
+             */
+            className={`${error ? "error" : null}`}
           ></input>
           <button className="btn" type="submit">
             Submit
